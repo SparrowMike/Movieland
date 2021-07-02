927c50e4769a2b5473f7fc1ecf9ab916e7cebb3f
sparrowmike <michal.m_wroblewski@hotmail.com>
2021-07-02 09:21:13 +0800

    going live test 2

--- src/components/Fetch.js
+++ src/components/Fetch.js
@@ -1,9 +1,6 @@
 import React, { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
 
 const Fetch = () => {
-  // const params = useParams();
-  // console.log(params);
   const [data, setData] = useState(null);
   let key = process.env.REACT_APP_API_KEY;
 
