d08e0c133fe2e5fa8501df7c5c031189382cc3e9
SparrowMike <69448825+SparrowMike@users.noreply.github.com>
2021-08-24 17:33:30 +0800

    refactor: 💡 folder name change

--- src/components/fetch/FetchMovies.js
+++ src/components/fetch/FetchMovies.js
@@ -1,44 +1,92 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
+import { useInfiniteQuery } from "react-query";
+import InfiniteScroll from "react-infinite-scroll-component";
 import { useParams } from "react-router";
 import Results from "../output/Results";
-import LoadingWindow from "../output/LoadingWindow";
+import { CircularProgress, Typography, Grid } from "@material-ui/core";
 
-const FetchSearch = () => {
-  let key = process.env.REACT_APP_API_KEY;
-  let type = "movie";
+export function FetchMovies() {
   const params = useParams();
+  let genre = "movie";
+  let key = process.env.REACT_APP_API_KEY;
+  function assertIsCharacterResponse(response) {
+    if (!("results" in response && "page" in response)) {
+      throw new Error("No results");
+    }
+    if (response.results.length > 0) {
+      const firstResult = response.results[0];
+      if (!("title" in firstResult)) {
+        throw new Error("No titles");
+      }
+    }
+  }
 
-  const [data, setData] = useState(null);
-  const [page, setPage] = useState(1);
+  async function getMovies({ pageParam = 1 }) {
+    const response = await fetch(
+      `https://api.themoviedb.org/3/discover/movie?api_key=${key}&language=en-US&sort_by=${params.sort}&certification_country=US&certification=${params.cert}&release_date.gte=${params.yearGte}-01-01&release_date.lte=${params.yearLte}-01-01&include_adult=false&include_video=false&page=${pageParam}&with_genres=${params.genre}&watch_region=${params.country}&with_runtime.gte=0&with_runtime.lte=200&with_watch_monetization_types=flatrate`
+    );
+    if (!response.ok) {
+      throw new Error("Problem fetching data");
+    }
+    const dataFromServer = await response.json();
+    assertIsCharacterResponse(dataFromServer);
+    const data = {
+      total_results: dataFromServer.total_results,
+      results: dataFromServer.results,
+      next:
+        dataFromServer.page === dataFromServer.total_pages ? 0 : pageParam + 1,
+    };
+    return data;
+  }
 
-  // const URL = `https://api.themoviedb.org/3/discover/movie?api_key=${key}&language=en-US&sort_by=${params.sort}&certification_country=US&certification=${params.cert}&release_date.gte=${params.yearGte}-01-01&release_date.lte=${params.yearLte}-01-01&include_adult=false&include_video=false&page=1&with_genres=${params.genre}&with_original_language=${params.lang}&watch_region=${params.country}&with_watch_monetization_types=flatrate`;
-  const URL = `https://api.themoviedb.org/3/discover/movie?api_key=${key}&language=en-US&sort_by=${params.sort}&certification_country=US&certification=${params.cert}&release_date.gte=${params.yearGte}-01-01&release_date.lte=${params.yearLte}-01-01&include_adult=false&include_video=false&page=${page}&with_genres=${params.genre}&watch_region=${params.country}&with_runtime.gte=0&with_runtime.lte=200&with_watch_monetization_types=flatrate`;
-
-  useEffect(() => {
-    fetch(URL)
-      .then((response) => {
-        if (response.ok) {
-          return response.json();
-        }
-        throw new Error("Bad Response from Server");
-      })
-      .then((data) => {
-        setData(data);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  }, [URL]);
-
-  return data === null ? (
-    <div>
-      <LoadingWindow />
-    </div>
-  ) : (
-    <div>
-      <Results data={data.results} key={key} type={type} setPage={setPage} />
-    </div>
+  const { data, fetchNextPage, hasNextPage } = useInfiniteQuery(
+    "movies",
+    getMovies,
+    {
+      getNextPageParam: (lastPage) => lastPage.next,
+    }
   );
-};
 
-export default FetchSearch;
+  console.log(data);
+  if (data === undefined) {
+    return null;
+  }
+  const dataLength = data.pages.reduce((counter, page) => {
+    return counter + page.results.length;
+  }, 0);
+
+  return (
+    <InfiniteScroll
+      dataLength={dataLength}
+      next={fetchNextPage}
+      hasMore={!!hasNextPage}
+      loader={
+        <Grid
+          container
+          className="loadingWindow"
+          direction="row"
+          justify="center"
+          alignItems="center"
+          style={{ padding: "75px" }}
+        >
+          {data.pages[0].total_results !== dataLength ? (
+            <>
+              <Typography variant="h3" color="primary">
+                Loading...
+              </Typography>
+              <CircularProgress color="primary" />
+            </>
+          ) : (
+            <Typography variant="h3" color="primary">
+              All up to date!
+            </Typography>
+          )}
+        </Grid>
+      }
+    >
+      {data.pages.map((data, index) => (
+        <Results data={data.results} key={index} genre={genre} />
+      ))}
+    </InfiniteScroll>
+  );
+}
---
+++ src/components/pages/Home.js
@@ -0,0 +1,112 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { useState } from "react";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
+import Container from "@material-ui/core/Container";
+import { Button } from "@material-ui/core";
+import Tabs from "@material-ui/core/Tabs";
+import Tab from "@material-ui/core/Tab";
+
+const useStyles = makeStyles((theme) => ({
+  textShadow: {
+    textShadow: "1px 1px 1px rgba(255, 255, 255, 0.4)",
+  },
+  button: {
+    margin: theme.spacing(3),
+  },
+
+  typographyHeader: {
+    [theme.breakpoints.down("xs")]: {
+      fontSize: "3rem",
+    },
+    [theme.breakpoints.up("md")]: {
+      fontSize: "5rem",
+    },
+  },
+}));
+
+export default function Home() {
+  const classes = useStyles();
+  const [type, setType] = useState("movie");
+  const [date, setDate] = useState("day");
+
+  const handleType = (e, newValue) => {
+    setType(newValue);
+  };
+
+  const handleDate = (e, newValue) => {
+    setDate(newValue);
+  };
+
+  const handleSubmit = () => {
+    setType(type);
+    setDate(date);
+  };
+
+  return (
+    <React.Fragment>
+      <div className="home">
+        <Container maxWidth="sm" className={classes.textShadow}>
+          <Typography
+            className={classes.typographyHeader}
+            component="h1"
+            variant="h2"
+            align="center"
+            color="primary"
+            gutterBottom
+          >
+            Welcome to MovieLand!
+          </Typography>
+          <Typography variant="h5" align="center" color="primary">
+            You are three clicks away to find trending Movies and TV Series!
+          </Typography>
+          <Tabs
+            value={type}
+            TabIndicatorProps={{
+              style: {
+                backgroundColor: "lightgrey",
+              },
+            }}
+            textColor="primary"
+            onChange={handleType}
+            centered
+          >
+            <Tab label="Movies" value={"movie"} />
+            <Tab label="or" disabled />
+            <Tab label="TV Series" value={"tv"} />
+          </Tabs>
+          <Tabs
+            TabIndicatorProps={{
+              style: {
+                backgroundColor: "lightgrey",
+              },
+            }}
+            value={date}
+            textColor="primary"
+            onChange={handleDate}
+            centered
+          >
+            <Tab label="Daily" value={"day"} />
+            <Tab label="Weekly" value={"week"} />
+          </Tabs>
+          <Link
+            style={{ textDecoration: "none" }}
+            to={`/trending/${type}/${date}`}
+          >
+            <Button
+              variant="contained"
+              className={classes.button}
+              color="primary"
+              disableElevation
+              size="large"
+              onClick={() => handleSubmit()}
+            >
+              Show Trending!
+            </Button>
+          </Link>
+        </Container>
+      </div>
+    </React.Fragment>
+  );
+}
